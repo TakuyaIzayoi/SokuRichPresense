1383762068987e1bc29e768215f011fe904687d9
Thiena <Thiena@iastate.edu>
2019-04-26 00:39:46 -0500

    fuck

--- sokuRichPresence.cpp
+++ sokuRichPresence.cpp
@@ -3,7 +3,9 @@
 #include <cstring>
 #include <cstdlib>
 #include <string>
+#include <ctime>
 
+#include <string.h>
 #include <windows.h>
 #include <mmsystem.h>
 #include <shlwapi.h>
@@ -37,6 +39,8 @@ static unsigned long long s_origCBattleManager_OnDestruct;
 static unsigned long long s_origCBattleManager_OnRender;
 static unsigned long long s_origCBattleManager_OnProcess;
 DiscordRichPresence discordPresence;
+DiscordEventHandlers handlers;
+
 
 #define ADDR_BMGR_P1 0x0C
 #define ADDR_BMGR_P2 0x10
@@ -48,7 +52,6 @@ DiscordRichPresence discordPresence;
 // OnRender is called before OnProcess, and loops.
 void InitDiscord()
 {
-    DiscordEventHandlers handlers;
     memset(&handlers, 0, sizeof(handlers));
     // handlers.ready = handleDiscordReady;
     // handlers.errored = handleDiscordError;
@@ -59,15 +62,18 @@ void InitDiscord()
 
     // Discord_Initialize(const char* applicationId, DiscordEventHandlers* handlers, int autoRegister, const char* optionalSteamId, int pipe)
     Discord_Initialize("570515970381185024", &handlers, 1, "1234");
+	
+	
+	memset(&discordPresence, 0, sizeof(discordPresence));
 }
 
 	//sends the update to the application/discord
-static void UpdatePresence()
-{
-    DiscordRichPresence discordPresence;
-    memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.state = "Yosu (Marisa)";
-    discordPresence.details = "Thiena (Aya)";
+static void NewPresence()
+{;
+
+	memset(&discordPresence, 0, sizeof(discordPresence));
+    // discordPresence.state = "Yosu (Marisa)";
+    // discordPresence.details = "Thiena (Aya)";
     discordPresence.largeImageKey = "sokuicon";
     discordPresence.largeImageText = "placeholder";
     // discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
@@ -82,13 +88,25 @@ void* __fastcall CBattleManager_OnCreate(void *This) {
 	CBattleManager_Create(This);
 	std::cout << "OnCreate Called: "<< sizeof(void* (C::*)()) << std::endl;
 
-	UpdatePresence();
+	NewPresence();
+	if (g_mainMode == SWRSMODE_PRACTICE)
+	{
+		discordPresence.details = "Practice Mode";
+	}
+	else
+	{
+		discordPresence.details = "Some Other Gamemode";
+	}
+	discordPresence.state = "Character Select";
+	Discord_UpdatePresence(&discordPresence);
 	return This;
 }
 
 	//loops 
 void __fastcall CBattleManager_OnRender(void *This) {
 	CBattleManager_Render(This);
+	
+
 }
 
 	//fights stuff? in battle.
@@ -96,11 +114,23 @@ int __fastcall CBattleManager_OnProcess(void *This) {
 	int ret;
 	ret = CBattleManager_Process(This);
 	int battleManager = ACCESS_INT(ADDR_BATTLE_MANAGER, 0);
+	
+	// discordPresence.state = "Yosu (Marisa)";
+    // discordPresence.details = "Thiena (Aya)";	
+	// discordPresence.startTimestamp = time(NULL);
+    // Discord_UpdatePresence(&discordPresence);
 
 	// Get address to the player data based on data inside "Battle Manager"
 	void* p1 = ACCESS_PTR(battleManager, ADDR_BMGR_P1);	
 	void* p2 = ACCESS_PTR(battleManager, ADDR_BMGR_P2);
 	
+	std::string wau = ACCESS_CHAR(p1, CF_CHARACTER_INDEX);
+	// char wau2 = ACCESS_CHAR(p1, CF_PLAYER_INDEX);
+
+	
+	// std::cout << wau << std::endl;
+	// std::cout << "OnCreate Called: "<< sizeof(void* (C::*)()) << std::endl;
+
 
 /* HEALTH DISPLAY */
 	//ACCESS_<variable_type>() is both used for accessing and writing to the resource.
@@ -113,23 +143,23 @@ int __fastcall CBattleManager_OnProcess(void *This) {
 
 	//Press CTRL, effect visible in VS mode, as practice mode hardsets HP 
 	if (GetKeyState(VK_CONTROL) & 0x8000) {
-		std::cout << "P1: " << p1Health << "(" << (float) (p1Spirit) / 200 << ")"
+		std::cout << "P1: " << p1Health << "(" << (float) (p1Spirit) / 200 << ")" << wau 
 			  << " [VS] P2: " << p2Health << "(" << (float) (p2Spirit) / 200 << ")" << std::endl;
-		ACCESS_SHORT(p1, CF_CURRENT_HEALTH) = 5000;
+		// ACCESS_SHORT(p1, CF_CURRENT_HEALTH) = 5000;
 	}
 
 
-/* GRAZING */
-	void* p1FrameData = ACCESS_PTR(p1, CF_CURRENT_FRAME_DATA);
-	void* p2FrameData = ACCESS_PTR(p2, CF_CURRENT_FRAME_DATA);
-	int p1FrameFlag = ACCESS_INT(p1FrameData, FF_FFLAGS);
-	int p2FrameFlag = ACCESS_INT(p2FrameData, FF_FFLAGS);
+// /* GRAZING */
+	// void* p1FrameData = ACCESS_PTR(p1, CF_CURRENT_FRAME_DATA);
+	// void* p2FrameData = ACCESS_PTR(p2, CF_CURRENT_FRAME_DATA);
+	// int p1FrameFlag = ACCESS_INT(p1FrameData, FF_FFLAGS);
+	// int p2FrameFlag = ACCESS_INT(p2FrameData, FF_FFLAGS);
 
-	// Flags are in fields.h, l.99 "Frame flags"
-	if (p1FrameFlag & FF_GRAZE) //binary operation on a bit to check if p1 is grazing or not.
-		std::cout << "-";
-	else
-		std::cout << " ";
+	// // Flags are in fields.h, l.99 "Frame flags"
+	// if (p1FrameFlag & FF_GRAZE) //binary operation on a bit to check if p1 is grazing or not.
+		// std::cout << "-";
+	// else
+		// std::cout << " ";
 
 
 	return ret;
@@ -140,6 +170,11 @@ void* __fastcall CBattleManager_OnDestruct(void *This, int mystery, int dyn) {
 	void* ret;
 	ret = CBattleManager_Destruct(This, dyn);
 	std::cout << "OnDestruct Called" << std::endl;
+	
+	NewPresence();
+	discordPresence.details = "Character Select";
+	Discord_UpdatePresence(&discordPresence);
+	
 
 	return ret;
 }
